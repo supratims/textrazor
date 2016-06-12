fa63523acd96f973bfd6c838ab7dd3cf694c18f9
supratim <supratims@gmail.com>
2016-06-12 10:53:12 +0100

    INITIAL commit with app keys and TextRazor implementation

--- client.php
+++ client.php
@@ -4,7 +4,6 @@ require_once('TextRazor.php');
 
 $text = 'Barclays misled shareholders and the public about one of the biggest investments in the banks history, a BBC Panorama investigation has found.';
 
-$YOUR_API_KEY
 $keys = parse_ini_file(__DIR__ . '/app.keys');
 $api_key = $keys['api.key'];
 $textrazor = new TextRazor($api_key);
